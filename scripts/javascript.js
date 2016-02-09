f07ef59e4ac9ad1afd33106d5536afefbef0f38a
DevanshV <dvaid96@gmail.com>
2016-02-08 22:42:01 -0500

    Implemented Firebase to record number of visitors

---
+++ scripts/javascript.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+	var myFirebaseRef = new Firebase("https://devansh.firebaseio.com/");
+
+	if (document.cookie.indexOf('devansh') === -1) {
+		document.cookie="devansh=true; expires=Thu, 03 Jan 2030 12:00:00 UTC; path=/";
+		myFirebaseRef.child("UniqueVisitors").once("value", function(snapshot) {
+			myFirebaseRef.update({UniqueVisitors: snapshot.val() + 1});
+		});	 
+	} else {
+		myFirebaseRef.child("TotalVisitors").once("value", function(snapshot) {
+			myFirebaseRef.update({TotalVisitors: snapshot.val() + 1});
+		});
+	}
+
+    $('.carousel').carousel();
+});
